ae9ff08d1c6c4c97735b13f283e47d7f74be4b4b
rhian9s <rhianelyse47@gmail.com>
2026-08-09 10:38:17 +0200

    Add checkBalance method to UssdService
    Refactor UssdService to add checkBalance method for USSD requests with permission handling.

--- lib/services/ussd_service.dart
+++ lib/services/ussd_service.dart
@@ -1,5 +1,18 @@
+import 'package:permission_handler/permission_handler.dart';
+import 'package:ussd_advanced/ussd_advanced.dart';
+
 class UssdService {
-  static Future<String> sendUssd(String code, int slot) async {
-    return "USSD: \$code SIM \${slot+1}";
+  static Future<String> checkBalance(int subId, String code) async {
+    // Ask permission first
+    if (await Permission.phone.request().isGranted) {
+      try {
+        String? result = await UssdAdvanced.sendUssd(code: code, subscriptionId: subId);
+        return result ?? "No response";
+      } catch (e) {
+        return "Error: $e";
+      }
+    } else {
+      return "Phone permission denied";
+    }
   }
 }
